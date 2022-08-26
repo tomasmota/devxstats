cab5473d6f2dd8502cbddfa9a3a38513b7725013
tomas-mota <tomasrebelomota@gmail.com>
2022-08-26 20:58:05 +0200

    refactor: use struct instead of bson

--- main.go
+++ main.go
@@ -5,23 +5,31 @@ import (
 	"fmt"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type Metric struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `bson:"name,omitempty"`
+	Description string             `bson:"description,omitempty"`
+}
 
 func main() {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
 
 	if err != nil {
 		panic(err)
-
 	}
 
+	defer client.Disconnect(context.TODO())
 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
 		panic(err)
 	}
 	metricsCollection := client.Database("devxmetrics").Collection("metrics")
+	defer metricsCollection.Drop(context.TODO())
 
 	fmt.Println("Creating metrics")
 	CreateMetrics(metricsCollection)
@@ -32,16 +40,35 @@ func main() {
 }
 
 func CreateMetrics(metricsCollection *mongo.Collection) {
-	users := []interface{}{
-		bson.D{{Key: "name", Value: "Deployment Frequency"}, {Key: "description", Value: "Rolling Average number of deployments per week over the past 2 months"}},
-		bson.D{{Key: "name", Value: "Lead Time"}, {Key: "description", Value: "Average duration between commits being pushed and being deployed to production"}},
-		bson.D{{Key: "name", Value: "Review Time"}, {Key: "description", Value: "Average time between a PR being created and being reviewed"}},
+	metrics := []interface{}{
+		Metric{
+			Name:        "Deployment Frequency",
+			Description: "Rolling Average number of deployments per week over the past 2 months",
+		},
+		Metric{
+			Name:        "Lead Time",
+			Description: "Average duration between commits being pushed and being deployed to production",
+		},
+		Metric{
+			Name:        "Review Time",
+			Description: "Average time between a PR being created and being reviewed",
+		},
 	}
 
-	_, err := metricsCollection.InsertMany(context.TODO(), users)
+	insertResult, err := metricsCollection.InsertMany(context.TODO(), metrics)
 	if err != nil {
 		panic(err)
 	}
+
+	contactIDs := insertResult.InsertedIDs
+
+	var contactIDs_ []primitive.ObjectID
+	for _, id := range contactIDs {
+		contactIDs_ = append(contactIDs_, id.(primitive.ObjectID))
+	}
+
+	fmt.Printf("Inserted %v %T\n", contactIDs_, contactIDs_)
+
 }
 
 func ReadMetrics(metricsCollection *mongo.Collection) {
@@ -60,13 +87,14 @@ func ReadMetrics(metricsCollection *mongo.Collection) {
 		panic(err)
 	}
 
-	var results []bson.M
-	if err = cursor.All(context.TODO(), &results); err != nil {
+	var metrics []Metric
+	if err = cursor.All(context.TODO(), &metrics); err != nil {
 		panic(err)
 	}
 
-	for _, result := range results {
-		fmt.Println(result)
+	fmt.Println("Matching Metrics:")
+	for _, metric := range metrics {
+		fmt.Println("  " + metric.Name + ": " + metric.Description)
 	}
 
 }
