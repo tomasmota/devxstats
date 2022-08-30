49adc15442d8542c23af1085a43a7cd77a51b7d2
Tomás Mota <tomasrebelomota@gmail.com>
2022-08-30 08:43:30 +0200

    Add properties to models

--- model/event.go
+++ model/event.go
@@ -7,24 +7,26 @@ import (
 )
 
 type Build struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	StartTime time.Time          `bson:"starttime,omitempty"`
-	EndTime   time.Time          `bson:"endtime,omitempty"`
-	Team      string             `bson:"team,omitempty"`
-	System    string             `bson:"system,omitempty"` // e.g. Tekton, Teamcity
-	Group     string             `bson:"group,omitempty"`  // tekton namespace, teamcity high-level project
-	Project   string             `bson:"repo,omitempty"`   // tekton pipeline, teamcity project
-	User      string             `bson:"user,omitempty"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	StartTime   time.Time          `bson:"starttime,omitempty"`
+	EndTime     time.Time          `bson:"endtime,omitempty"`
+	Team        string             `bson:"team,omitempty"`
+	System      string             `bson:"system,omitempty"` // e.g. Tekton, Teamcity
+	Group       string             `bson:"group,omitempty"`  // tekton namespace, teamcity high-level project
+	Project     string             `bson:"repo,omitempty"`   // tekton pipeline, teamcity project
+	User        string             `bson:"user,omitempty"`
+	Succeeded   bool          `bson:"succeeded,omitempty"`
 }
 
 type PullRequest struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Timestamp time.Time          `bson:"timestamp,omitempty"`
-	Team      string             `bson:"team,omitempty"`
-	System    string             `bson:"system,omitempty"` // e.g. Github, Bitbucket
-	Group     string             `bson:"group,omitempty"`  // Github org, Bitbucket project
-	Repo      string             `bson:"repo,omitempty"`
-	User      string             `bson:"user,omitempty"`
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
+	OpenTime   time.Time          `bson:"timestamp,omitempty"`
+	CloseTime  time.Time          `bson:"timestamp,omitempty"`
+	Team       string             `bson:"team,omitempty"`
+	System     string             `bson:"system,omitempty"` // e.g. Github, Bitbucket
+	Group      string             `bson:"group,omitempty"`  // Github org, Bitbucket project
+	Repo       string             `bson:"repo,omitempty"`
+	User       string             `bson:"user,omitempty"`
 }
 
 type Commit struct {
@@ -38,11 +40,12 @@ type Commit struct {
 }
 
 type Deployment struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	StartTime time.Time          `bson:"starttime,omitempty"`
-	EndTime   time.Time          `bson:"endtime,omitempty"`
-	Team      string             `bson:"team,omitempty"`
-	System    string             `bson:"system,omitempty"`
-	Group     string             `bson:"group,omitempty"`
-	Project   string             `bson:"project,omitempty"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	StartTime   time.Time          `bson:"starttime,omitempty"`
+	EndTime     time.Time          `bson:"endtime,omitempty"`
+	Team        string             `bson:"team,omitempty"`
+	System      string             `bson:"system,omitempty"`
+	Group       string             `bson:"group,omitempty"`
+	Project     string             `bson:"project,omitempty"`
+	Succeeded   bool          `bson:"succeeded,omitempty"`
 }
