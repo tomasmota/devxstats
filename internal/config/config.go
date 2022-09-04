3b62f62d72eafea9f0da60b47b41c8919723d401
tomas-mota <tomasrebelomota@gmail.com>
2022-09-04 20:35:48 +0200

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+type AppConfig struct {
+	Port int       `env:"PORT,default=8080"`
+	Db   *DbConfig `env:",prefix=DB_"`
+	Git  *GitConfig
+	Cd   *CdConfig
+}
+
+type DbConfig struct {
+	Host string `env:"HOST,required"`
+	Port int    `env:"PORT,default=27017"`
+}
+
+type GitConfig struct {
+	Github struct {
+		Token string `env:"GITHUB_TOKEN"`
+		Url   string `env:"GITHUB_URL"`
+	}
+	Bitbucket struct {
+		Url   string `env:"BITBUCKET_URL"`
+		Token string `env:"BITBUCKET_TOKEN"`
+	}
+}
+
+type CdConfig struct {
+	Octopus struct {
+		Token string `env:"OCTOPUS_TOKEN"`
+		Url   string `env:"OCTOPUS_URL"`
+	}
+}
