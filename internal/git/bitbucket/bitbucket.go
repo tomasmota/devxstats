7a99f3fa70e858c6756e4475c858e4713d6ae550
Thomas Mota <tmm@danskecommodities.com>
2022-09-09 15:03:02 +0200

    more testing (WIP)

--- internal/git/bitbucket/bitbucket.go
+++ internal/git/bitbucket/bitbucket.go
@@ -40,23 +40,15 @@ func NewBitbucketClient(config *BitbucketConfig) (*bitbucketClient, error) {
 func (c *bitbucketClient) GetOpenPullRequests(ctx context.Context) ([]*model.PullRequest, error) {
 	fmt.Println("Fetching bitbucket open pull requests")
 
-	opts := scm.ListOptions{
-		Page: 1,
-		Size: 300,
-	}
+	var prCount, repoCount int
+	page := 1
 
-	repos, res, err := c.Client.Repositories.List(ctx, opts)
-	if err != nil {
-		return nil, fmt.Errorf("error fetching repositories: %v", err)
-	}
-	if res.Status != 200 {
-		return nil, fmt.Errorf("error fetching repositories, received status: %v", res.Status)
-	}
-
-	fmt.Printf("found %d repos\n", len(repos))
-	var count int
-	for _, r := range repos {
-		prs, res, err := c.Client.PullRequests.List(ctx, fmt.Sprintf("%v/%v", r.Namespace, r.Name), scm.PullRequestListOptions{Open: true})
+	for {
+		opts := scm.ListOptions{
+			Page: page,
+			Size: 1000,
+		}
+		repos, res, err := c.Client.Repositories.List(ctx, opts)
 		if err != nil {
 			return nil, fmt.Errorf("error fetching repositories: %v", err)
 		}
@@ -64,10 +56,29 @@ func (c *bitbucketClient) GetOpenPullRequests(ctx context.Context) ([]*model.Pul
 			return nil, fmt.Errorf("error fetching repositories, received status: %v", res.Status)
 		}
 
-		fmt.Printf("%v open pull requests in repo %v\n", len(prs), r.Name)
-		count += len(prs)
+		page = res.Page.Next
+		repoCount += len(repos)
+
+		for _, r := range repos {
+			prs, res, err := c.Client.PullRequests.List(ctx, fmt.Sprintf("%v/%v", r.Namespace, r.Name), scm.PullRequestListOptions{Open: true})
+			if err != nil {
+				return nil, fmt.Errorf("error fetching repositories: %v", err)
+			}
+			if res.Status != 200 {
+				return nil, fmt.Errorf("error fetching repositories, received status: %v", res.Status)
+			}
+
+			if len(prs) > 0 {
+				fmt.Printf("%v open pull requests in repo %v\n", len(prs), fmt.Sprintf("%v/%v", r.Namespace, r.Name))
+			}
+			prCount += len(prs)
+		}
+
+		if res.Page.Next == 0 {
+			break
+		}
 	}
-	fmt.Printf("found %d open pull requests\n", count)
+	fmt.Printf("found %d open pull requests across %d repos\n", prCount, repoCount)
 
 	prs := []*scm.PullRequest{{}} // TODO: fetch prs here
 	return convertPullRequests(prs...), nil
