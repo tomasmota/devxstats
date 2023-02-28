2c0f28d761df15b0244f28bc516042c2cd951674
tomasmota <tomasrebelomota@gmail.com>
2023-02-28 14:10:18 +0100

    comment out sync
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- cmd/devxstats/main.go
+++ cmd/devxstats/main.go
@@ -5,30 +5,27 @@ import (
 	"devxstats/internal/api"
 	"devxstats/internal/config"
 	"devxstats/internal/db"
-	"devxstats/internal/source/cd"
-	"devxstats/internal/source/git"
 
 	"fmt"
-	"time"
 )
 
 // Fetches events from all sources and stores them on a loop
-func syncSources(ctx context.Context, c config.AppConfig, db db.DB) {
-	git := git.NewGitSyncer(c.Git, db)
-	cd := cd.NewCdSyncer(c.Cd, db)
-
-	// for {
-	err := git.Sync(ctx)
-	if err != nil {
-		panic(fmt.Errorf("error syncing git sources: %w", err))
-	}
-	err = cd.Sync(ctx)
-	if err != nil {
-		panic(fmt.Errorf("error syncing cd sources: %w", err))
-	}
-	time.Sleep(5000 * time.Millisecond)
-	// }
-}
+// func syncSources(ctx context.Context, c config.AppConfig, db db.DB) {
+// 	git := git.NewGitSyncer(c.Git, db)
+// 	cd := cd.NewCdSyncer(c.Cd, db)
+//
+// 	// for {
+// 	err := git.Sync(ctx)
+// 	if err != nil {
+// 		panic(fmt.Errorf("error syncing git sources: %w", err))
+// 	}
+// 	err = cd.Sync(ctx)
+// 	if err != nil {
+// 		panic(fmt.Errorf("error syncing cd sources: %w", err))
+// 	}
+// 	time.Sleep(5000 * time.Millisecond)
+// 	// }
+// }
 
 func main() {
 	ctx := context.Background()
@@ -40,7 +37,7 @@ func main() {
 	db := db.InitPostgres(ctx)
 
 	// Start syncing sources in the background
-	go syncSources(ctx, *c, db)
+	// go syncSources(ctx, *c, db)
 
 	// Initialize http server
 	s := api.NewHTTPServer(db)
