be4f64bd618b393c0759dccd14a74df95ffbe841
tomasmota <tomasrebelomota@gmail.com>
2023-05-28 21:10:05 +0200

    add pg to flake
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- flake.nix
+++ flake.nix
@@ -15,18 +15,21 @@
       let
         pkgs = import nixpkgs { inherit system ; };
       in
-      {
-        defaultPackage = pkgs.buildGoModule {
+      rec {
+        packages.devxstats = pkgs.buildGoModule {
           name = "devxstats";
           src = gitignore.lib.gitignoreSource ./.;
           vendorSha256 = "sha256-KpBviY+m0iOiWPxVpqBYAjYzXsb7mfPSmwhc3/yxF28=";
         };
+
+        packages.default = packages.devxstats;
 
         devShell = pkgs.mkShellNoCC {
           packages = with pkgs; [
             go_1_20
             gotools
             gopls
+            postgresql_15
           ];
         };
       }
